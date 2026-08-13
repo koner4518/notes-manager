2577cad1e3595ea4ae2fc532bf7218a50833366e
Supriyo Koner <supriyokoner2005@gmail.com>
2026-08-13 14:02:28 +0530

    Add user registration

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+import userRouter from "./routes/authRoute.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/auth", userRouter);
 
 app.get("/", (req, res) => {
     res.send("Notes Manager API is running");
